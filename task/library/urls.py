7e9ec25fbf325d0c5785493f709d67030961c144
dzmitrybutar <verysweetleaf@gmail.com>
2019-08-09 13:33:07 +0300

    Created user_detail view

--- task/library/urls.py
+++ task/library/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.users_list, name='users_list'),
+    path('user/<int:pk>/', views.user_details, name='user_detail_url'),
 ]
--- task/library/views.py
+++ task/library/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-from .models import User
+from django.shortcuts import render, get_object_or_404
+from .models import User, Book
+
 
 def users_list(request):
     users = User.objects.all()
     return render(request, 'users_list.html', {'users': users})
+
+
+def user_details(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    books = Book.objects.filter(user=user)
+    return render(request, 'user_details.html', {'books': books, 'user': user})
