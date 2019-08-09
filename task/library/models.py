b0e1439f3b6fe5d1246c9641b0e814679ace1032
dzmitrybutar <verysweetleaf@gmail.com>
2019-08-09 12:30:24 +0300

    Created User and Book models

--- task/library/models.py
+++ task/library/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.Model):
+    user = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.user
+
+
+class Book(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    pages = models.IntegerField()
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.title} - {self.author} ({self.publication_data})'
