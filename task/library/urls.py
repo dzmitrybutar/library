ab5aa26e293b494ec2b480baa2427059b2d4f04c
dzmitrybutar <verysweetleaf@gmail.com>
2019-08-09 14:04:03 +0300

    Refactoring views and created form view

--- task/library/urls.py
+++ task/library/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-
+from .views import UserListView
 
 urlpatterns = [
-    path('', views.users_list, name='users_list'),
+    path('', UserListView.as_view(), name='users_list'),
     path('user/<int:pk>/', views.user_details, name='user_detail_url'),
 ]
--- task/library/views.py
+++ task/library/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import User, Book
+from django.views import View
+from .forms import BookForm, UserForm
 
 
-def users_list(request):
-    users = User.objects.all()
-    return render(request, 'users_list.html', {'users': users})
+class UserListView(View):
+
+    def get(self, request):
+        context = {
+            'users': User.objects.all(),
+            'form': UserForm()
+        }
+        return render(request, 'users_list.html', context=context)
+
+    def post(self, request):
+        context = {
+            'users': User.objects.all(),
+            'form': UserForm()
+        }
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            return redirect('users_list')
+        return render(request, 'users_list.html', context=context)
 
 
 def user_details(request, pk):
